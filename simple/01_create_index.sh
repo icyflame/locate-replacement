7c8326803646497dc2afa1d3a86ad0cd642090d0
Siddharth Kannan <mail@siddharthkannan.in>
2022-07-26 19:11:29 +0900

    Explain the exclude list; look at only files inside the home directory
    The directories to look in should be easily configurable

--- simple/01_create_index.sh
+++ simple/01_create_index.sh
@@ -17,7 +17,15 @@ then
 fi
 
 rm -f $TEMP_INDEX_FILE
-fd '.*' --exclude vendor --exclude node_modules > $TEMP_INDEX_FILE /
+
+# Exclude list
+# - vendor: Golang vendor files
+# - pkg: Golang compilation output of source
+# - node_modules: Node.js dependencies
+# - /System: Apple's Filesystem has a firmlink between /System/Volumes/Data/Users/... and /Users/...
+#    - "firmlink" object is similar to symlink
+#    - https://devstreaming-cdn.apple.com/videos/wwdc/2019/710aunvynji5emrl/710/710_whats_new_in_apple_file_systems.pdf
+fd '.*' --type f --type d --exclude vendor --exclude pkg --exclude node_modules --exclude /System > $TEMP_INDEX_FILE $HOME
 
 INDEX_FILE_BAK=$INDEX_FILE.bak
 rm -f $INDEX_FILE_BAK
