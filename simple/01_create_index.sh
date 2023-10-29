d279ab5dbabbe0a23f144cd1520de7f644b893a0
Siddharth Kannan <mail@siddharthkannan.in>
2023-10-29 15:13:17 +0900

    Supply index path as an argument

--- simple/01_create_index.sh
+++ simple/01_create_index.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-DIRNAME=$(dirname $0)
+TEMP_INDEX_FILE="/tmp/temp-index-file"
 
-source $DIRNAME/vars
-
-if [ ! -x $(which fd) ];
+INDEX_FILE=$1
+if [[ -z "$INDEX_FILE" ]];
 then
-	echo "fd is required"
+    cat <<EOF
+ERROR: First argument must be a path to the index file
+
+Example:
+
+    $0 INDEX_FILE_PATH
+EOF
+    exit 41
+fi
+
+if [[ ! -x $(which fd) && ! -x $(which fdfind) ]];
+then
+	echo "fd or fdfind is required"
 	exit 42
+fi
+
+if [[ -x $(which fd) ]];
+then
+    EXECUTABLE=$(which fd)
+else
+    EXECUTABLE=$(which fdfind)
 fi
 
 if [ -z "$HOME" ];
@@ -25,7 +43,7 @@ rm -f $TEMP_INDEX_FILE
 # - /System: Apple's Filesystem has a firmlink between /System/Volumes/Data/Users/... and /Users/...
 #    - "firmlink" object is similar to symlink
 #    - https://devstreaming-cdn.apple.com/videos/wwdc/2019/710aunvynji5emrl/710/710_whats_new_in_apple_file_systems.pdf
-fd '.*' --type f --type d --exclude vendor --exclude pkg --exclude node_modules --exclude /System > $TEMP_INDEX_FILE $HOME
+$EXECUTABLE '.*' --type f --type d --exclude vendor --exclude pkg --exclude node_modules --exclude /System > $TEMP_INDEX_FILE $HOME
 
 INDEX_FILE_BAK=$INDEX_FILE.bak
 rm -f $INDEX_FILE_BAK
