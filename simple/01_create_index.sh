088a24b1ec917302e24f7e8b168c8925c961dc2f
Siddharth Kannan <mail@siddharthkannan.in>
2023-10-29 15:15:57 +0900

    Specify root search directory as well

--- simple/01_create_index.sh
+++ simple/01_create_index.sh
@@ -10,15 +10,28 @@ ERROR: First argument must be a path to the index file
 
 Example:
 
-    $0 INDEX_FILE_PATH
+    $0 INDEX_FILE_PATH [ROOT_DIRECTORY_FOR_SEARCH]
 EOF
     exit 41
+fi
+
+ROOT_FOR_SEARCH=${2:-$HOME}
+if [ -z "$ROOT_FOR_SEARCH" ];
+then
+    cat <<EOF
+ERROR: ROOT_FOR_SEARCH directory must be defined
+
+Example:
+
+    $0 INDEX_FILE_PATH [ROOT_DIRECTORY_FOR_SEARCH]
+EOF
+	exit 42
 fi
 
 if [[ ! -x $(which fd) && ! -x $(which fdfind) ]];
 then
 	echo "fd or fdfind is required"
-	exit 42
+	exit 43
 fi
 
 if [[ -x $(which fd) ]];
@@ -26,12 +39,6 @@ then
     EXECUTABLE=$(which fd)
 else
     EXECUTABLE=$(which fdfind)
-fi
-
-if [ -z "$HOME" ];
-then
-	echo "Home directory must be defined"
-	exit 44
 fi
 
 rm -f $TEMP_INDEX_FILE
@@ -43,13 +50,6 @@ rm -f $TEMP_INDEX_FILE
 # - /System: Apple's Filesystem has a firmlink between /System/Volumes/Data/Users/... and /Users/...
 #    - "firmlink" object is similar to symlink
 #    - https://devstreaming-cdn.apple.com/videos/wwdc/2019/710aunvynji5emrl/710/710_whats_new_in_apple_file_systems.pdf
-$EXECUTABLE '.*' --type f --type d --exclude vendor --exclude pkg --exclude node_modules --exclude /System > $TEMP_INDEX_FILE $HOME
-
-INDEX_FILE_BAK=$INDEX_FILE.bak
-rm -f $INDEX_FILE_BAK
-if [ -f $INDEX_FILE ];
-then
-	cp $INDEX_FILE $INDEX_FILE_BAK
-fi
+$EXECUTABLE '.*' --type f --type d --exclude vendor --exclude pkg --exclude node_modules --exclude /System > $TEMP_INDEX_FILE $ROOT_FOR_SEARCH
 
 mv $TEMP_INDEX_FILE $INDEX_FILE
